c718604add5964d25e369f0a28116ce13e9800df
youcef_quant <youcefkouzi63@gmail.com>
2026-07-10 14:14:06 +0100

    Create handle_missing_data.py

---
+++ missing-data-handler/handle_missing_data.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+stock1=yf.download("AAPL",
+                  start="2020-01-01",
+                  end="2026-01-01")
+df=stock1[["Close"]].rename(columns={"Close":"Cpi"})
+np.random.seed(42)
+history1=np.random.choice(df.index,
+                          5,
+                          replace=False)
+df["cpi_missing"]=df.loc[:, "Cpi"]
+df.loc[history1,"cpi_missing"]=np.nan
+df[df["cpi_missing"].isna()]
+
+
+df["method_ffill"] = df["cpi_missing"].ffill()
+df["method_bfill"] = df["cpi_missing"].bfill()
+df[df["cpi_missing"].isna()]
