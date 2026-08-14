aff8a2dcd0b04570f96c4790f2e4115fa22e614c
youcef_quant <youcefkouzi63@gmail.com>
2026-08-14 20:13:54 +0100

    Create candlestick_chart.py

---
+++ candlestick_chart/candlestick_chart.py
@@ -0,0 +1,25 @@
+import mplfinance as mpf
+import pandas as pd
+import yfinance as yf
+
+df = yf.download("AAPL", period="6mo", progress=False)
+
+if isinstance(df.columns, pd.MultiIndex):
+  df.columns = df.columns.droplevel(1)
+
+custom_style = mpf.make_mpf_style(
+    base_mpf_style="charles",
+    facecolor="black",
+    figcolor="black",
+    gridcolor="#333333",
+    gridstyle="--",
+)
+
+mpf.plot(
+    df,
+    style=custom_style,
+    type="candle",
+    volume=True,
+    title="AAPL Candlestick Chart",
+    mav=(10, 20),
+)
