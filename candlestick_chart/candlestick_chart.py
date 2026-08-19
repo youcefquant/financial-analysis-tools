c45541730844cb3809bb36347f90d568c61da81e
youcef_quant <youcefkouzi63@gmail.com>
2026-08-19 18:56:51 +0100

    Update stock data source to TSLA
    Changed stock data source from AAPL to TSLA and adjusted download parameters.

--- candlestick_chart/candlestick_chart.py
+++ candlestick_chart/candlestick_chart.py
@@ -2,13 +2,11 @@ import mplfinance as mpf
 import pandas as pd
 import yfinance as yf
 
-df = yf.download("AAPL",
-                 period="6mo", 
+df = yf.download("TSLA",
+                 period="6mo",
+                 multi_level_index=False,
                  progress=False)
 
-if isinstance(df.columns,
-              pd.MultiIndex):
-  df.columns = df.columns.droplevel(1)
 
 custom_style = mpf.make_mpf_style(
     base_mpf_style="charles",
